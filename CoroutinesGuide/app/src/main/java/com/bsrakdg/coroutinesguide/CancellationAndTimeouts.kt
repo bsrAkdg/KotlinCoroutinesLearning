d2ced6c95512ee108e36b5521cf0e33a7f41e697
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-08-13 14:19:34 +0300

    Run non-cancellable block

--- CoroutinesGuide/app/src/main/java/com/bsrakdg/coroutinesguide/CancellationAndTimeouts.kt
+++ CoroutinesGuide/app/src/main/java/com/bsrakdg/coroutinesguide/CancellationAndTimeouts.kt
@@ -26,6 +26,10 @@ fun main() {
     // TODO Closing resources with finally
     closingResourcesWithFinally()
 
+    println("\n****************************\n")
+
+    // TODO Run non-cancellable block
+    runNonCancellableBlock()
 }
 
 fun cancellingCoroutineExecution() {
@@ -171,4 +175,44 @@ fun closingResourcesWithFinallySample() = runBlocking {
         Both join and cancelAndJoin wait for all finalization actions to complete,
          so the example above produces the following output:
      */
+}
+
+fun runNonCancellableBlock() {
+
+    /*
+        Any attempt to use a suspending function in the finally block of the previous example causes
+        CancellationException, because the coroutine running this code is cancelled.
+        Usually, this is not a problem, since all well-behaving closing operations
+        (closing a file, cancelling a job, or closing any kind of a communication channel) are
+        usually non-blocking and do not involve any suspending functions. However,
+        in the rare case when you need to suspend in a cancelled coroutine you can wrap
+        the corresponding code in withContext(NonCancellable) {...} using withContext function and
+        NonCancellable context as the following example shows:
+     */
+
+    runNonCancellableBlockSample()
+}
+fun runNonCancellableBlockSample() = runBlocking {
+    println("runNonCancellableBlockSample start")
+
+    val job = launch {
+        try {
+            repeat(1000) { i ->
+                println("job: I'm sleeping $i ...")
+                delay(500L)
+            }
+        } finally {
+            withContext(NonCancellable) {
+                println("job: I'm running finally")
+                delay(1000L)
+                println("job: And I've just delayed for 1 sec because I'm non-cancellable")
+            }
+        }
+    }
+    delay(1300L) // delay a bit
+    println("main: I'm tired of waiting!")
+    job.cancelAndJoin() // cancels the job and waits for its completion
+    println("main: Now I can quit.")
+
+    println("runNonCancellableBlockSample end")
 }

11e480107e65461a11e6f2051fbe8539231fb0bc
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-08-13 22:32:30 +0300

    Lazily started async

--- CoroutinesGuide/app/src/main/java/com/bsrakdg/coroutinesguide/ComposingSuspendingFunctions.kt
+++ CoroutinesGuide/app/src/main/java/com/bsrakdg/coroutinesguide/ComposingSuspendingFunctions.kt
@@ -1,5 +1,6 @@
 package com.bsrakdg.coroutinesguide
 
+import kotlinx.coroutines.CoroutineStart
 import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
@@ -21,7 +22,10 @@ suspend fun main() {
 
     println("\n****************************\n")
 
+    // TODO Lazily started async
+    lazilyStartedAsync()
 
+    println("\n****************************\n")
 }
 
 suspend fun sequentialByDefault() {
@@ -96,5 +100,43 @@ fun concurrentUsingAsyncSample() = runBlocking {
         This is twice as fast, because the two coroutines execute concurrently.
         Note that concurrency with coroutines is always explicit.
      */
+}
 
+fun lazilyStartedAsync() {
+    /*
+        Optionally, async can be made lazy by setting its start parameter to CoroutineStart.LAZY.
+        In this mode it only starts the coroutine when its result is required by await,
+        or if its Job's start function is invoked. Run the following example:
+     */
+
+    lazilyStartedAsyncSample()
+}
+
+fun lazilyStartedAsyncSample()  = runBlocking {
+    println("lazilyStartedAsyncSample start")
+
+    val time = measureTimeMillis {
+        val one = async(start = CoroutineStart.LAZY) { doSomethingUsefulOne() }
+        val two = async(start = CoroutineStart.LAZY) { doSomethingUsefulTwo() }
+        // some computation
+        one.start() // start the first one
+        two.start() // start the second one
+        println("The answer is ${one.await() + two.await()}")
+    }
+    println("Completed in $time ms")
+
+    println("lazilyStartedAsyncSample end")
+
+    /*
+        So, here the two coroutines are defined but not executed as in the previous example,
+        but the control is given to the programmer on when exactly to start the execution by calling start.
+        We first start one, then start two, and then await for the individual coroutines to finish.
+
+        Note that if we just call await in println without first calling start on individual coroutines,
+        this will lead to sequential behavior, since await starts the coroutine execution and waits for its finish,
+        which is not the intended use-case for laziness.
+
+        The use-case for async(start = CoroutineStart.LAZY) is a replacement for the standard
+        lazy function in cases when computation of the value involves suspending functions.
+     */
 }
